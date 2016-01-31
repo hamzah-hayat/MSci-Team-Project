9fca5f3b8cf5d2f58a473720b9694a2fe218b175
Filip Tér <filip.ter@gmail.com>
2016-01-31 19:02:52 +0000

    Some refactoring - Added some interface methods - Cleaned up some code

--- app/src/main/java/com/group/msci/puzzlegenerator/maze/CLIMain.java
+++ app/src/main/java/com/group/msci/puzzlegenerator/maze/CLIMain.java
@@ -9,17 +9,17 @@ public class CLIMain {
 
 
     public static void main(String[] args) throws InterruptedException{
-        BaseMaze m = new BaseMaze(21, 21);
-        MazeTimer timer = new MazeTimer(3000, 1000, m);
-        timer.start();
-        m.log();
-        Thread.sleep(3001);
-        //PortalMaze pm = new PortalMaze(21, 21, 4);
+        //BaseMaze m = new BaseMaze(21, 21);
+        //MazeTimer timer = new MazeTimer(3000, 1000, m);
+        //timer.start();
+        //m.log();
+        //Thread.sleep(3001);
+        PortalMaze pm = new PortalMaze(21, 21, 4);
         //pm.log();
         //pm.logMat();
-        //test(new BaseMaze(21, 21));
-        //test(new PortalMaze(21, 21, 4));
-        //test(new Maze3D(21));
+        test(new BaseMaze(21, 21));
+        test(new PortalMaze(21, 21, 4));
+        test(new Maze3D(21));
     }
 
     public static void test(Maze maze) {
--- app/src/main/java/com/group/msci/puzzlegenerator/maze/Maze.java
+++ app/src/main/java/com/group/msci/puzzlegenerator/maze/Maze.java
@@ -4,8 +4,11 @@ package com.group.msci.puzzlegenerator.maze;
  * Created by Filipt on 19/01/2016.
  */
 public interface Maze {
-
     void solve();
     void log();
     void regenerate();
+    byte at(Point coords);
+    Point playerPos();
+    boolean movePlayer(int direction);
+    boolean solved();
 }
